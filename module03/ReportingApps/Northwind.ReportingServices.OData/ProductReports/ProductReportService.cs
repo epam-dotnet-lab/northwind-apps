f1b7434d5bab9aa1eda3cb4c596159d3cff26eaa
Alexander <19323417+alexander-rykov@users.noreply.github.com>
2020-02-02 13:44:28 +0300

    Update module02 and module 03.

--- module03/ReportingApps/Northwind.ReportingServices.OData/ProductReports/ProductReportService.cs
+++ module03/ReportingApps/Northwind.ReportingServices.OData/ProductReports/ProductReportService.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Services.Client;
-using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
 using NorthwindProduct = NorthwindModel.Product;
